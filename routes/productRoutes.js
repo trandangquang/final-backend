c81ecda82684d58fc2506c0a5251d6b170089a4f
trandangquang <trandangquang2203@gmail.com>
2023-10-21 16:23:47 +0700

    update product routes & server

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,6 +4,7 @@ const User = require('../models/User');
 
 router.get('/', async (req, res) => {
   try {
+    // const sort = { _id: -1 };
     const products = await Product.find();
     res.status(200).json(products);
   } catch (e) {
@@ -40,6 +41,7 @@ router.patch('/:id', async (req, res) => {
       pictures,
     });
     const products = await Product.find();
+    console.log('product-from patch', products);
     res.status(200).json(products);
   } catch (e) {
     res.status(400).send(e.message);
@@ -61,14 +63,34 @@ router.delete('/:id', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  const { id } = req.params;
+  const {id} = req.params;
+  console.log('id', id)
   try {
     const product = await Product.findById(id);
-    const similar = await Product.find({ category: product.category }).limit(5);
-    res.status(200).json(product, similar);
+    console.log('product from get', product);
+    res.status(200).json({product});
   } catch (e) {
     res.status(400).send(e.message);
   }
 });
+
+router.get('/category/:category', async (req, res) => {
+  const { category } = req.params;
+  try {
+    let products;
+    const sort = { _id: -1 };
+    if (category == 'all') {
+      products = await Product.find().sort(sort);
+    } else {
+      products = await Product.find({ category }).sort(sort);
+    }
+    res.status(200).json(products);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
+
+
 
 module.exports = router;

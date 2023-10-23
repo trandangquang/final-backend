e070fa46cc66e8d8595c3fc869aa35b43d2716ba
trandangquang <trandangquang2203@gmail.com>
2023-10-23 19:38:14 +0700

    update product routes

--- models/Product.js
+++ models/Product.js
@@ -1,26 +1,29 @@
 const mongoose = require('mongoose')
-const ProductSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Cannot be left blank'],
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Cannot be left blank'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Cannot be left blank'],
+    },
+    price: {
+      type: String,
+      required: [true, 'Cannot be left blank'],
+    },
+    category: {
+      type: String,
+      required: [true, 'Cannot be left blank'],
+    },
+    pictures: {
+      type: Array,
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: [true, 'Cannot be left blank'],
-  },
-  price: {
-    type: String,
-    required: [true, 'Cannot be left blank'],
-  },
-  category: {
-    type: String,
-    required: [true, 'Cannot be left blank'] ,
-  },
-  pictures: {
-    type: Array,
-    required: true
-  }
-},{minimize: false});
+  { minimize: false }
+);
 
 const Product = mongoose.model('Product', ProductSchema)
 module.exports = Product
--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const http = require('http')
-const bodyParser = require('body-parser')
-require('./connection')
-const server = http.createServer(app)
-const {Server} = require('socket.io')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const http = require('http');
+const bodyParser = require('body-parser');
+require('./connection');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: 'http://localhost:3001',
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
 });
 
-const User = require('./models/User')
-const userRoutes = require('./routes/userRoutes')
-const productRoutes = require('./routes/productRoutes')
-const imageRoutes = require('./routes/imageRoutes')
-const orderRoutes = require('./routes/orderRoutes',)
+const User = require('./models/User');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const imageRoutes = require('./routes/imageRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
-app.use(cors())
-app.use(express.urlencoded({extended: true}))
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.json())
-app.use('/users',userRoutes)
-app.use('/products', productRoutes)
-app.use('/images', imageRoutes)
+app.use(express.json());
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/images', imageRoutes);
 app.use('/orders', orderRoutes);
 
 
 
-server.listen(8080, ()=> {
-  console.log('Server is running at port', 8080 )
-})
+server.listen(8080, () => {
+  console.log('Server is running at port', 8080);
+});
